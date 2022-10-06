d0a06609ecced7e0a1550321459269cb4de356c9
Steffen Hageland <etse88@gmail.com>
2022-10-06 14:14:51 +0200

    Endrer til rett font

--- docs/slides.tsx
+++ docs/slides.tsx
@@ -1,27 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { Deck } from 'spectacle'
-import { TitleSplash } from './slides/TitleSplash'
-import { PresentereOss } from './slides/PresentereOss'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './fonts/fonts.css';
+import { Deck } from 'spectacle';
+import { TitleSplash } from './slides/TitleSplash';
+import { PresentereOss } from './slides/PresentereOss';
 
 const theme = {
     fonts: {
-        header: "'DejaVu Sans', Helvetica, sans-serif",
-        text: "'DejaVu Sans', Helvetica, sans-serif",
+        header: 'Newzald, Georgia, sans-serif',
+        text: "DIN, 'Calibre Light', sans-serif",
     },
     colors: {
         primary: '#FFFFFF',
         secondary: '#FFFFFF',
         tertiary: '#0E0E0E',
     },
-}
+};
 
 const Presentation = () => (
     <Deck theme={theme}>
         <TitleSplash />
         <PresentereOss />
     </Deck>
-)
+);
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<Presentation />)
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Presentation />);
