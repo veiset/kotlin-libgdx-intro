c67c7e104ef710c49c9906815d28c386617a3694
Steffen Hageland <etse88@gmail.com>
2022-10-07 10:15:52 +0200

    Deploy docs til github pages

--- docs/vite.config.js
+++ docs/vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+const BASE_URL = process.env.BASE_URL || '/';
+
 export default defineConfig({
     plugins: [react()],
+    base: BASE_URL,
 });
