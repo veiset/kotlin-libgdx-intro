1db4194fa50c1f8af62bc13c8f206a8de1df98ba
Steffen Hageland <etse88@gmail.com>
2022-10-27 10:36:06 +0200

    Oppdatert slides med de som deltar

--- docs/slides/PresentereOss.tsx
+++ docs/slides/PresentereOss.tsx
@@ -1,38 +1,135 @@
 import React from 'react';
-import { SlideLayout, Text, FlexBox, Image, fadeTransition } from 'spectacle';
+import {
+    SlideLayout,
+    Text,
+    FlexBox,
+    Image,
+    fadeTransition,
+    Heading,
+} from 'spectacle';
 import vegardPNG from './bilder/vegard.png';
-import steffenPNG from './bilder/steffen.png';
+import erikaPNG from './bilder/erika.png';
+import sigurdPNG from './bilder/sigurd.png';
+import augustPNG from './bilder/august.png';
+import frikkPNG from './bilder/frikk.png';
+import joakimPNG from './bilder/joakim.png';
+import kenanPNG from './bilder/kenan.png';
+import ingridPNG from './bilder/ingrid.png';
+import tormartinPNG from './bilder/tormartin.png';
+import styled from '@emotion/styled';
 
 type OmPersonProps = {
     navn: string;
+    erfaring: string;
     bildeUrl: string;
 };
 
-const OmPerson = ({ navn, bildeUrl }: OmPersonProps) => (
-    <FlexBox flexDirection="column" justifyContent="center" height="100%">
-        <Image width="300px" alt={`Bilde av ${navn}`} src={bildeUrl} />
-        <Text
-            fontWeight="300"
-            textAlign="center"
-            padding="0px"
-            mt="60px"
-            fontSize={25}
-        >
-            {navn}
-        </Text>
-    </FlexBox>
+const OmPerson = ({ navn, bildeUrl, erfaring }: OmPersonProps) => (
+    <PersonWrapper>
+        <FlexBox flexDirection="column" justifyContent="center">
+            <RoundedProfileImage bildeUrl={bildeUrl} />
+            <Text
+                fontWeight="300"
+                textAlign="center"
+                padding="0px"
+                margin="10px"
+                fontSize={20}
+            >
+                {navn}
+            </Text>
+            <Text
+                fontWeight="300"
+                textAlign="center"
+                padding="0px"
+                margin="0px"
+                fontSize={16}
+            >
+                {erfaring} i Bekk
+            </Text>
+        </FlexBox>
+    </PersonWrapper>
 );
 
 export const PresentereOss = () => (
     <SlideLayout.Full transition={fadeTransition}>
-        <FlexBox
-            flexDirection="row"
-            height="100%"
-            justifyContent="space-evenly"
-            alignItems="center"
-        >
-            <OmPerson navn="Steffen Hageland" bildeUrl={steffenPNG} />
-            <OmPerson navn="Vegard Veiset" bildeUrl={vegardPNG} />
-        </FlexBox>
+        <Heading>Hvem er vi?</Heading>
+        <MainWrapper>
+            <OmPerson
+                navn="Erika Åsberg"
+                bildeUrl={erikaPNG}
+                erfaring="2 måneder"
+            />
+            <OmPerson
+                navn="Sigurd Hynne"
+                bildeUrl={sigurdPNG}
+                erfaring="2 måneder"
+            />
+            <OmPerson navn="August Dahl" bildeUrl={augustPNG} erfaring="1 år" />
+            <OmPerson
+                navn="Frikk Andersen"
+                bildeUrl={frikkPNG}
+                erfaring="1 år"
+            />
+            <OmPerson
+                navn="Joakim Sjøhaug"
+                bildeUrl={joakimPNG}
+                erfaring="1 år"
+            />
+            <OmPerson navn="Kenan Mahic" bildeUrl={kenanPNG} erfaring="2 år" />
+            <OmPerson
+                navn="Ingrid Bolden"
+                bildeUrl={ingridPNG}
+                erfaring="2 år"
+            />
+            <OmPerson
+                navn="Vegard Veiset"
+                bildeUrl={vegardPNG}
+                erfaring="9 år"
+            />
+            <OmPerson
+                navn="Tor Martin Wang"
+                bildeUrl={tormartinPNG}
+                erfaring="2 måneder"
+            />
+        </MainWrapper>
     </SlideLayout.Full>
 );
+
+type RoundedProfileImageProps = {
+    bildeUrl: string;
+};
+
+const RoundedProfileImage = ({ bildeUrl }: RoundedProfileImageProps) => (
+    <ProfileImageWrapper>
+        <ProfileImage src={bildeUrl} />
+    </ProfileImageWrapper>
+);
+
+const ProfileImageWrapper = styled.div`
+    width: 180px;
+    height: 180px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    overflow: hidden;
+    border-radius: 50%;
+`;
+
+const ProfileImage = styled(Image)`
+    max-height: 220px;
+    max-width: 220px;
+`;
+
+const MainWrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+    width: 100%;
+    max-width: 95vw;
+`;
+
+const PersonWrapper = styled.div`
+    width: 19%;
+    margin-top: 10px;
+    margin-bottom: 30px;
+`;
