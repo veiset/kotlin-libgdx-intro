12f2376dc5b803598ce23ad0bc88ccab6327a38d
gauteab <gaute.berge@gmail.com>
2023-05-24 15:39:05 +0200

    wip om dataklasser og immutability

--- src/main/kotlin/no/bekk/introduction/Intro.kt
+++ src/main/kotlin/no/bekk/introduction/Intro.kt
@@ -1,10 +1,31 @@
 package no.bekk.introduction
 
-class Konsulent(val name: String)
-
-// Klasse konsulent
+data class Konsulent(val name: String)
 
 fun main() {
-    // Print konsulent
-    // Flere statements kommentert ut
+    val gaute = Konsulent("Gaute")
+    // sjekk hvilke egenskaper og funksjoner som finnes på en `Konsulent`
+    // merk at vi ikke trenger getter?
+    println(gaute)
+    // Endre til data class - hvilke egenskaper får du opp nå?
+    // vi får copy, og
+
+
+
+    // Prøv å endre navnet til konsulenten
+
+    /**
+    gaute.name = "Sondre"
+    // */
+
+    /**
+    val sondre = gaute.copy(name = "Sondre")
+    println(sondre)
+    // */
+
+    // I standardbiblioteket til kotlin skiller man på datastrukturer som kan endres direkte (muterbare), og de hvor man ikke kan det
+    // feks finnes det både `List<T>` og `MutableList<T>` som begge er generiske lister, men `List` implementerer ikke funksjoner som `add` og `remove`.
+    listOf(1,2,3)
+    mutableListOf(1)
+
 }
